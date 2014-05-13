a3bb1f9b58f0d74a0de8313c20dc4941df3f0962
koomahnah <koomahnah@gmail.com>
2014-05-13 20:56:44 +0200

    timer one shot

--- source/timer.c
+++ source/timer.c
@@ -4,13 +4,15 @@
 #include "irq.h"
 
 volatile unsigned int *timercs = (unsigned int *)TIMERCS;
-void (*extTIrqHandler)(void) = 0;
+void (*extTIrqHandler)(void);
+void (*tIrqHandler)(void);
 static unsigned int timerMatch;
 void timerInit()
 {
 	*irqEnable1=2;
 	irqEnable();
 	extTIrqHandler = 0;
+	tIrqHandler = tCircHandler;
 //	unsigned int tmp = *timercs;
 //	tmp |= (1<<5)|(1<<7);
 }
@@ -24,12 +26,12 @@ void timerSetMatch(unsigned int value)
 	*timercs = 0b10;
 	timerMatch = value;
 }
-void tIrqHandler(void){
-//	irqDisableSec();
+
+void timerOneShot(unsigned int value, 
+void tCircHandler(void){
 	unsigned int tmp = *(timercs+1);
 	tmp+=timerMatch;
 	*(timercs+4)=tmp;
 	*timercs=0b10;
 	if(extTIrqHandler!=0) extTIrqHandler();
-//	irqEnableSec();
 }
--- source/timer.h
+++ source/timer.h
@@ -2,7 +2,11 @@
 
 void timerInit();
 void timerSetMatch(unsigned int value);
-void tIrqHandler(void);
+void timerOneShot(unsigned int value, void (*pFunct)(void));
+void (*__timerPFunct)(void);
+void tCircHandler();
+void tOneShotHandler();
+extern void (*tIrqHandler)(void);
 
 extern void (*extTIrqHandler)(void);
 extern volatile unsigned int *timercs;
